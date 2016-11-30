a375c2921514890278c301b28b008c9723a10933
hungry <277862831@qq.com>
2016-11-30 21:53:53 +0800

    add description to InsertSort

--- 基本排序/InsertSort.cpp
+++ 基本排序/InsertSort.cpp
@@ -2,19 +2,31 @@
 
 using namespace std;
 
-//插入排序
+/**************************************************************
+
+Author：hungry
+Date:2016.11.30
+Description:插入排序
+
+Analysis:
+① 类似于扑克牌的整理牌的顺序，从第二个牌开始
+② 从当前牌的前一个牌开始遍历，逆序找到不大于当前牌的位置，并将大于该牌的往后“挤”
+③ 将该牌放置到该位置
+
+**************************************************************/
+
 static void insertSort(int* lists,int length)
 {
 	for (size_t i = 1; i < length; i++)
 	{
-		int temp = lists[i];
+		auto curr = lists[i];
 
 		size_t j;
 
-		for (j = i; j > 0 && lists[j-1] > temp; j--)
+		for (j = i; j > 0 && lists[j - 1] > curr; j--)
 			lists[j] = lists[j - 1];
 
-		lists[j] = temp;
+		lists[j] = curr;
 	}
 
 	for (size_t i = 0; i < length; i++)
